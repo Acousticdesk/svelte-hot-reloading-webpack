12331f970b386bbe4c059887c451e6796eb09a4e
Andrii Kicha <acousticdesk@gmail.com>
2020-06-09 10:03:59 +0300

    add webpack config

---
+++ components/App.svelte
@@ -0,0 +1,1 @@
+<p>Hello from App component!</p>
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: './main.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /.svelte$/,
+        use: 'svelte-loader',
+      }
+    ],
+  },
+};
