c001924ec261a4da772a5482026c46ec69a3853e
Andrii Kicha <acousticdesk@gmail.com>
2020-06-10 09:17:06 +0300

    add hot reloading feature for the app

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,22 @@ const path = require('path');
 module.exports = {
   entry: './main.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    publicPath: '/build',
+    compress: true,
+    port: 9001,
+    hot: true,
   },
   module: {
     rules: [
       {
         test: /.svelte$/,
-        use: 'svelte-loader',
+        loader: 'svelte-loader',
+        options: { hotReload: true },
       }
     ],
   },
